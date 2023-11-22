ab36af7616608541407debf5f8463dedea09109b
RafGeorges <raf.georges@gmail.com>
2023-11-22 16:51:46 +0100

    s

--- backend/app.js
+++ backend/app.js
@@ -41,4 +41,12 @@ app.use((req, res, next) => {
       .catch(error => res.status(400).json({ error }));
   });
 
+
+//  GET 
+  app.get('/api/books/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+      .then(thing => res.status(200).json(thing))
+      .catch(error => res.status(404).json({ error }));
+  });
+
 module.exports = app;
